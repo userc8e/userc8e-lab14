0d5a6fa15daddf3488aeada8a595fe6d648c74b1
userc8e <caitlynquack@gmail.com>
2025-11-10 14:41:05 -0500

    added skeleton for Client.java

--- Client.java
+++ Client.java
@@ -1,3 +1,27 @@
+import java.net.*;
 public class Client {
+    Socket sock;
+    String hostName;
+    int portNum;
+
+    public Client(String hostName, int portNum) {
+        this.hostName = hostName;
+        this.portNum = portNum;
+    }
     
+    public Socket getSocket() {
+        return sock;
+    }
+
+    public void handshake() {
+
+    }
+
+    public void disconnect() {
+
+    }
+
+    public String request(int num) {
+        
+    }
 }
