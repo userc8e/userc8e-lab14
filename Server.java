6e9dc452354b2c3078888a6b4d747015bb21701b
userc8e <caitlynquack@gmail.com>
2025-11-10 14:42:49 -0500

    added Server skeleton

--- Server.java
+++ Server.java
@@ -1,3 +1,19 @@
 public class Server {
-    
+    private int portNum;
+
+    public Server(int portNum) {
+        this.portNum = portNum;
+    }
+
+    public void serve() {
+
+    }
+
+    public void serve(int n) {
+
+    }
+
+    public void disconnect() {
+        
+    }
 }
