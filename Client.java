bb061785f5916a40f133436d26c90d2da8e83372
userc8e <caitlynquack@gmail.com>
2025-11-17 00:21:57 -0500

    client constructor

--- Client.java
+++ Client.java
@@ -1,12 +1,14 @@
+import java.io.*;
 import java.net.*;
 public class Client {
     private Socket sock;
     private String hostName;
     private int portNum;
 
-    public Client(String hostName, int portNum) {
+    public Client(String hostName, int portNum) throws IOException {
         this.hostName = hostName;
         this.portNum = portNum;
+        this.sock = new Socket(hostName, portNum);
     }
     
     public Socket getSocket() {
