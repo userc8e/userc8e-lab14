fdefa562ef5a028e7ae4211c7b483b5724df19bd
userc8e <caitlynquack@gmail.com>
2025-11-17 00:44:10 -0500

    client methods

--- Client.java
+++ Client.java
@@ -16,14 +16,38 @@ public class Client {
     }
 
     public void handshake() {
-
+        try {
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            out.println("12345");
+        } catch (IOException e) {
+            System.err.println("Error during handshake:");
+            e.printStackTrace();
+        }
     }
 
     public void disconnect() {
-
+        try {
+            if (sock != null && !sock.isClosed()) {
+                sock.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Error while disconnecting:");
+            e.printStackTrace();
+        }
     }
 
     public String request(String s) {
-
+        String response = null;
+        try {
+            PrintWriter writer = new PrintWriter(sock.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            
+            writer.println(s);
+            response = reader.readLine();
+        } catch (IOException e) {
+            System.err.println("Error during request:");
+            e.printStackTrace();
+        }
+        return response;
     }
 }
